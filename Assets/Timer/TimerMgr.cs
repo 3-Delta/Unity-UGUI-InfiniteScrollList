9d9b8b2ea4d6b421322bcd64a897cc0956e29bff
Cui3 <kaclok@users.noreply.github.com>
2021-04-16 01:45:18 +0800

    修改timer

--- Assets/Timer/TimerMgr.cs
+++ Assets/Timer/TimerMgr.cs
@@ -21,16 +21,36 @@ using Object = UnityEngine.Object;
 /// This will be instantiated the first time you create a timer -- you do not need to add it into the
 /// scene manually.
 /// </summary>
-public static class TimerMgr
-{
+public static class TimerMgr {
     private static List<MonoTimer> timers = new List<MonoTimer>();
-    // ����Ϊ��ʹ���������Ļ���ȥ��¼������timer��ԭ���ǣ���foreach�У�ÿ��timerִ�еĹ����У����ֱ������һ������times���ͻᵼ������������ʧЧ��
-    // ����һ�㶼���ȵ�����timersȫ��foreach��ϣ�Ȼ���ȥ�����µ�timer
     private static List<MonoTimer> timersToAdd = new List<MonoTimer>();
 
     public static void Register(MonoTimer timer) { timersToAdd.Add(timer); }
-    public static void CancelAll()
-    {
+    public static void UnRegister(MonoTimer timer) {
+        int index = timers.IndexOf(timer);
+        int lastIndex = 0;
+        if (index != -1) {
+            if (timers.Count > 1) {
+                lastIndex = timers.Count - 1;
+                if (index != lastIndex) {
+                    timers[index] = timers[lastIndex];
+                }
+            }
+            else {
+                timers.RemoveAt(lastIndex);
+            }
+        }
+
+        index = timersToAdd.IndexOf(timer);
+        if (index == -1) {
+            lastIndex = timersToAdd.Count - 1;
+            if (index != lastIndex) {
+                timersToAdd[index] = timersToAdd[lastIndex];
+            }
+            timersToAdd.RemoveAt(lastIndex);
+        }
+    }
+    public static void CancelAll() {
         timers.ForEach(timer => { timer.Cancel(); });
         timers.Clear();
         timersToAdd.Clear();
@@ -38,10 +58,8 @@ public static class TimerMgr
     public static void PauseAll() { timers.ForEach(timer => { timer.Pause(); }); }
     public static void ResumeAll() { timers.ForEach(timer => { timer.Resume(); }); }
     public static void OnUpdate() { UpdateAll(); }
-    private static void UpdateAll()
-    {
-        if (timersToAdd.Count > 0)
-        {
+    private static void UpdateAll() {
+        if (timersToAdd.Count > 0) {
             timers.AddRange(timersToAdd);
             timersToAdd.Clear();
         }
